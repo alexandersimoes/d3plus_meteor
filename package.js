4fb319f054c5f1c13ba9ddda621b52cd2adddb74
Dave Landry <landry.dave@gmail.com>
2015-01-29 20:39:29 -0500

    fixed git repo and removed d3 dependency

--- package.js
+++ package.js
@@ -1,11 +1,10 @@
 Package.describe({
-  git: 'https://github.com/alexandersimoes/d3plus.git',
+  git: 'https://github.com/alexandersimoes/d3plus_meteor.git',
   name: 'simoes:d3plus',
   summary: 'A javascript library that extends the popular D3.js to enable fast and beautiful visualizations.',
   version: '0.1.2'
 });
 
 Package.on_use(function (api) {
-  api.use('d3js:d3', 'client');
-  api.add_files('d3plus/d3plus.js', 'client');
+  api.add_files('d3plus/d3plus.full.min.js', 'client');
 });
