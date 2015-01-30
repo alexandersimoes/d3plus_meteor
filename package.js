ee0afdd334c9a47dd498ed928696ce0480531912
Dave Landry <landry.dave@gmail.com>
2015-01-29 20:12:24 -0500

    added name and version

--- package.js
+++ package.js
@@ -1,6 +1,8 @@
 Package.describe({
   git: 'https://github.com/alexandersimoes/d3plus.git',
-  summary: 'A javascript library that extends the popular D3.js to enable fast and beautiful visualizations.'
+  name: 'simoes:d3plus',
+  summary: 'A javascript library that extends the popular D3.js to enable fast and beautiful visualizations.',
+  version: '0.1.2'
 });
 
 Package.on_use(function (api) {
