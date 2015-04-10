39b14eafee4977ba1504c520f99ea1a354e67526
Dave Landry <landry.dave@gmail.com>
2015-04-10 00:13:44 -0400

    v1.7.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: 'https://github.com/alexandersimoes/d3plus_meteor.git',
   name: 'simoes:d3plus',
   summary: 'A javascript library that extends the popular D3.js to enable fast and beautiful visualizations.',
-  version: '1.6.9'
+  version: '1.7.0'
 });
 
 Package.on_use(function (api) {
